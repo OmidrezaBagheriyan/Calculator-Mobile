bb8e9c69a409da47698b835523ceef15fe325145
OmidrezaBagheriyan <OmidrezaBagheriyan@gmail.com>
2023-09-21 23:35:48 +0330

    Implementation dependencies & config compose in build.gradle.kts components

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.omidrezabagheriyan.components"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -30,14 +30,42 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Compose dependencies
+    val activityComposeVersion = "1.7.2"
+    implementation("androidx.activity:activity-compose:$activityComposeVersion")
+    val composeBomVersion = "2023.03.00"
+    val platform = platform("androidx.compose:compose-bom:$composeBomVersion")
+    implementation(platform)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    androidTestImplementation(platform)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Basic dependencies
+    val coreVersion = "1.12.0"
+    implementation("androidx.core:core-ktx:$coreVersion")
+    val appCompatVersion = "1.6.1"
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    val materialVersion = "1.9.0"
+    implementation("com.google.android.material:material:$materialVersion")
+    val junitVersion = "4.13.2"
+    testImplementation("junit:junit:$junitVersion")
+    val extJunitVersion = "1.1.5"
+    androidTestImplementation("androidx.test.ext:junit:$extJunitVersion")
+    val espressoCoreVersion = "3.5.1"
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoCoreVersion")
 }
